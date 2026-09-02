1bb327ce87c8d1e53c9cd32c4de5c02bf4107b1e
matifl3 <matifelissia2205@gmail.com>
2026-09-01 23:23:28 -0300

    animaciones front

--- lfmNacional/src/main/java/org/example/lfmnacional/dto/usuario/CambioPasswordRequest.java
+++ lfmNacional/src/main/java/org/example/lfmnacional/dto/usuario/CambioPasswordRequest.java
@@ -3,7 +3,7 @@ package org.example.lfmnacional.dto.usuario;
 import jakarta.validation.constraints.NotBlank;
 
 public record CambioPasswordRequest(
-        @NotBlank String passwordActual,
+        String passwordActual,
         @NotBlank String nuevaPassword
 ) {
 }
